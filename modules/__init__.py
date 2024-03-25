44e41c10ac57b46494a15400813391590936162a
Aleksander Drønnen <aleksander@viddal.net>
2024-03-25 17:09:40 +0100

    working gauges

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,4 +3,3 @@
 from .stuff import ImageSprite, PlaceObject
 from .gauges import Gauge, RPMGauge
 from .segment_display import SevenSegmentDisplay, SevenSegmentClock
-from .rpm_gauge import RPMGauge
